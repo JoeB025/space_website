ea4374b1b9b56d4891e586fa4d6a98bee13d8991
Joseph Scott-Bentley <josephscott-bentley@macbookpro.home>
2025-02-23 17:18:15 +0000

    Set DATABASE_URL and NODE_ENV environment variables

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,25 @@
+// const { Pool } = require('pg');
+// const ENV = process.env.NODE_ENV || 'development';
+
+// require('dotenv').config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
+
+// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+//   throw new Error('PGDATABASE not set or DATABASE_URL not set');
+// }
+
+
+// const config = {}
+
+// if (ENV === 'production') {
+//   config.connectionString = process.env.DATABASE_URL;
+//   config.max = 2;
+// }
+
+
+// module.exports = new Pool(config);
+
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
@@ -5,17 +27,24 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+
+console.log('ENV:', ENV);
+console.log('PGDATABASE:', process.env.PGDATABASE);
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set or DATABASE_URL not set');
 }
 
-
-const config = {}
+const config = {};
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  config.ssl = {
+    rejectUnauthorized: false
+  };
+} else {
+  config.database = process.env.PGDATABASE;
 }
-
 
 module.exports = new Pool(config);
